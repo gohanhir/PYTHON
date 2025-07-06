dc838b46cedd488c31e976880c5e535d16ce06e1
MacBook Pro 13 M1 <macbook@MacBooks-MacBook-Pro.local>
2025-07-06 20:30:07 +0500

    done

---
+++ py/mini_projects/convert_sum.py
@@ -0,0 +1,15 @@
+print("1-sum to usd")
+print("2-usd to sum")
+
+choice = input()
+
+rate = 12500
+
+if choice == "1":
+    sums = float(input("sum: "))
+    dollars = sums/rate
+    print(sums, "sums is", dollars, "dollars")
+elif choice == "2":
+    dollars = float(input("dollars: "))
+    sums = dollars*rate
+    print(dollars, "dollars is", sums, "sums")
